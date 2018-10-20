8a654474583833a022dbf994a9826fb585363b59
Igor Danich <dclife2009@gmail.com>
2018-10-21 01:15:18 +0300

    update

--- DCUtils/Classes/Log.swift
+++ DCUtils/Classes/Log.swift
@@ -7,21 +7,29 @@ import Foundation
 fileprivate var sharedLog: NSObject?
 fileprivate var logExist = true
 
-public enum Log {}
-
-extension Log {
+public enum Logs {
+    public class Weak {
+        
+        static let shared = Logs.Weak()
+        
+        fileprivate init() {}
     
-    public static func weak(tag: String? = nil, category: String, priority: Int = 1000, parameters: [String:Any]?) {
-        if logExist && sharedLog == nil {
-            guard let cls = NSClassFromString("LogObjC") as? NSObject.Type else { logExist = false; return }
-            sharedLog = cls.self.init()
+        func append(tag: String? = nil, category: String, priority: Int = 1000, parameters: [String:Any]?) {
+            if logExist && sharedLog == nil {
+                guard let cls = NSClassFromString("LogObjC") as? NSObject.Type else { logExist = false; return }
+                sharedLog = cls.self.init()
+            }
+            var record = [String:Any]()
+            record["tag"] = tag
+            record["category"] = category
+            record["priority"] = priority
+            record["parameters"] = parameters
+            sharedLog?.perform(NSSelectorFromString("append:"), with: record)
         }
-        var record = [String:Any]()
-        record["tag"] = tag
-        record["category"] = category
-        record["priority"] = priority
-        record["parameters"] = parameters
-        sharedLog?.perform(NSSelectorFromString("append:"), with: record)
+        
     }
-    
+}
+
+func Log() -> Logs.Weak {
+    return Logs.Weak.shared
 }
--- TestApp-iOS/ViewController.swift
+++ TestApp-iOS/ViewController.swift
@@ -3,6 +3,7 @@
 //
 
 import UIKit
+import DCUtils
 
 class ViewController: UIViewController {
 
